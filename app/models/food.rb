f133e98ba646dc4f494cf03323ad5115c822acf5
Bettenoch <bettenoch254@gmail.com>
2023-11-21 20:15:01 +0300

    create food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,9 @@
 class Food < ApplicationRecord
+    belongs_to :user
+    has_many :recipefoods, dependent: :destroy
+    has_many :recipes, through: :recipe_foods, dependent: :destroy
+  
+    validates :name, presence: true, length: { maximum: 100 }
+    validates :measurement_unit, presence: true, length: { maximum: 50 }
+    validates :price, :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
