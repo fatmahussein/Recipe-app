364f7108bb6f1c75248bcc0e0615351b0e78a459
fatmahussein <fartunhussein346@gmail.com>
2023-11-24 08:05:10 +0300

    Add basic configurations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_110306) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_075449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,14 +25,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_110306) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table "recipefoods", force: :cascade do |t|
+  create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "recipe_id"
     t.bigint "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_recipefoods_on_food_id"
-    t.index ["recipe_id"], name: "index_recipefoods_on_recipe_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -51,10 +51,21 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_110306) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
-  add_foreign_key "recipefoods", "foods"
-  add_foreign_key "recipefoods", "recipes"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
