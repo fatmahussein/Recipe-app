b67079decef2fb03fe95a9968bdf3bd1f1d51d1b
fatmahussein <fartunhussein346@gmail.com>
2023-11-24 07:41:09 +0300

    Add device authentication.

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    can :read, Recipe, user_id: user.id
+
+    can :manage, Food, user_id: user.id unless user.nil?
+    can :manage, Recipe, user_id: user.id unless user.nil?
+   
+  end
+end
