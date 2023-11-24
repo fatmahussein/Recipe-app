5e001ffcde9dc7c7a15f89e28a0db404374ecc65
Bettenoch <bettenoch254@gmail.com>
2023-11-24 07:47:04 +0300

    add food controller new create and index methods.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,11 +1,33 @@
 class FoodsController < ApplicationController
   def index
-    @user = User.find(params[:id])
-    @food = @user.food
+    @user = current_user
+    @foods = @user.foods
   end
 
   def show
     @user = User.find(params[:user_id])
-    @food = @user.food.find(params[:id])
+    @food = @user.foods.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @user = current_user
+    @food = Food.new(foods_param)
+    @food.user = @user
+
+    if @food.save
+      flash[:notice] = 'Food was created successfully.'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Oops, something went wrong'
+      render :new
+    end
+  end
+
+  def foods_param
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
